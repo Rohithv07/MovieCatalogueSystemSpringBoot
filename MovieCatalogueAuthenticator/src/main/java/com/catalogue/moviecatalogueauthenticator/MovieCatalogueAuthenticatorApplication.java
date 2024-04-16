3f35ac6f1947be0c273fea64193406b2a19bc818
priyaganga98 <priyaganga98@gmail.com>
2024-04-17 01:17:11 +0530

    initial spring boot application

---
+++ MovieCatalogueAuthenticator/src/main/java/com/catalogue/moviecatalogueauthenticator/MovieCatalogueAuthenticatorApplication.java
@@ -0,0 +1,13 @@
+package com.catalogue.moviecatalogueauthenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogueAuthenticatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCatalogueAuthenticatorApplication.class, args);
+    }
+
+}
---
+++ MovieCatalogueAuthenticator/src/test/java/com/catalogue/moviecatalogueauthenticator/MovieCatalogueAuthenticatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalogue.moviecatalogueauthenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogueAuthenticatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

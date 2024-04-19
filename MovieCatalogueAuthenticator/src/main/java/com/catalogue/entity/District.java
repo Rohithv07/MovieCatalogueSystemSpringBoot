d2f4087fc3667e38aad7f9ee3408928e9fad4182
Rohithv07 <rohithv63@gmail.com>
2024-04-20 00:20:21 +0530

    Added entities address

--- MovieCatalogueAuthenticator/src/main/java/com/catalogue/entity/District.java
+++ MovieCatalogueAuthenticator/src/main/java/com/catalogue/entity/District.java
@@ -6,49 +6,49 @@ import jakarta.persistence.*;
 @Table(name = "DISTRICT")
 public class District {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "DISTRICT_ID")
-    private long districtId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "DISTRICT_ID")
+	private long districtId;
 
-    @Column(name = "DISTRICT_NAME", length = 30)
-    private String districtName;
+	@Column(name = "DISTRICT_NAME", length = 30)
+	private String districtName;
 
-    @ManyToOne
-    @JoinColumn(name = "STATE_ID", referencedColumnName = "STATE_ID")
-    private State stateId;
+	@ManyToOne
+	@JoinColumn(name = "STATE_ID", referencedColumnName = "STATE_ID")
+	private State state;
 
-    public long getDistrictId() {
-        return districtId;
-    }
+	public long getDistrictId() {
+		return districtId;
+	}
 
-    public void setDistrictId(long districtId) {
-        this.districtId = districtId;
-    }
+	public void setDistrictId(long districtId) {
+		this.districtId = districtId;
+	}
 
-    public String getDistrictName() {
-        return districtName;
-    }
+	public String getDistrictName() {
+		return districtName;
+	}
 
-    public void setDistrictName(String districtName) {
-        this.districtName = districtName;
-    }
+	public void setDistrictName(String districtName) {
+		this.districtName = districtName;
+	}
 
-    public State getStateId() {
-        return stateId;
-    }
+	public State getState() {
+		return state;
+	}
 
-    public void setStateId(State stateId) {
-        this.stateId = stateId;
-    }
+	public void setState(State state) {
+		this.state = state;
+	}
 
-    public District() {
-    }
+	public District() {
+	}
 
-    public District(long districtId, String districtName, State stateId) {
-        super();
-        this.districtId = districtId;
-        this.districtName = districtName;
-        this.stateId = stateId;
-    }
+	public District(long districtId, String districtName, State state) {
+		super();
+		this.districtId = districtId;
+		this.districtName = districtName;
+		this.state = state;
+	}
 }
--- MovieCatalogueAuthenticator/src/main/java/com/catalogue/entity/State.java
+++ MovieCatalogueAuthenticator/src/main/java/com/catalogue/entity/State.java
@@ -6,49 +6,49 @@ import jakarta.persistence.*;
 @Table(name = "STATE")
 public class State {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "STATE_ID")
-    private long stateId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "STATE_ID")
+	private long stateId;
 
-    @Column(name = "STATE_NAME", length = 30)
-    private String stateName;
+	@Column(name = "STATE_NAME", length = 30)
+	private String stateName;
 
-    @ManyToOne
-    @JoinColumn(name = "COUNTRY_ID", referencedColumnName = "COUNTRY_ID")
-    private Country countryId;
+	@ManyToOne
+	@JoinColumn(name = "COUNTRY_ID", referencedColumnName = "COUNTRY_ID")
+	private Country country;
 
-    public long getStateId() {
-        return stateId;
-    }
+	public long getStateId() {
+		return stateId;
+	}
 
-    public void setStateId(long stateId) {
-        this.stateId = stateId;
-    }
+	public void setStateId(long stateId) {
+		this.stateId = stateId;
+	}
 
-    public String getStateName() {
-        return stateName;
-    }
+	public String getStateName() {
+		return stateName;
+	}
 
-    public void setStateName(String stateName) {
-        this.stateName = stateName;
-    }
+	public void setStateName(String stateName) {
+		this.stateName = stateName;
+	}
 
-    public Country getCountryId() {
-        return countryId;
-    }
+	public Country getCountry() {
+		return country;
+	}
 
-    public void setCountryId(Country countryId) {
-        this.countryId = countryId;
-    }
+	public void setCountry(Country country) {
+		this.country = country;
+	}
 
-    public State() {
-    }
+	public State() {
+	}
 
-    public State(long stateId, String stateName, Country countryId) {
-        super();
-        this.stateId = stateId;
-        this.stateName = stateName;
-        this.countryId = countryId;
-    }
+	public State(long stateId, String stateName, Country country) {
+		super();
+		this.stateId = stateId;
+		this.stateName = stateName;
+		this.country = country;
+	}
 }

1c3de6f50b48f977fe4af8146e83c376935b73f8
Rohithv07 <rohithv63@gmail.com>
2024-05-01 22:51:06 +0530

    Added basic structure and starting files

---
+++ MovieCatalogueAuthenticator/src/main/java/com/catalogue/exception/MovieCatalogException.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.catalogue.exception;
+
+/**
+ * @author rohithvazhathody
+ *
+ */
+public class MovieCatalogException {
+
+	/**
+	 * 
+	 */
+	public MovieCatalogException() {
+	}
+
+}

81615f30669e12bf289a97f9051560db032e2997
priyaganga98 <priyaganga98@gmail.com>
2024-08-06 23:44:35 +0530

    remove unused import

--- MovieCatalogueAuthenticator/src/main/java/com/catalogue/moviecatalogueauthenticator/repositories/StateRepository.java
+++ MovieCatalogueAuthenticator/src/main/java/com/catalogue/moviecatalogueauthenticator/repositories/StateRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.catalogue.moviecatalogueauthenticator.entity.State;
-import org.springframework.data.jpa.repository.Query;
 
 /**
  * @author priya prasad

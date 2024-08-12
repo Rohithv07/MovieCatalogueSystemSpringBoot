58f2b2af218a8a2d33cab0f16bf4dc1b382c69a8
priyaganga98 <priyaganga98@gmail.com>
2024-08-12 23:20:06 +0530

    userdto

--- MovieCatalogueAuthenticator/src/main/java/com/catalogue/moviecatalogueauthenticator/services/UserService.java
+++ MovieCatalogueAuthenticator/src/main/java/com/catalogue/moviecatalogueauthenticator/services/UserService.java
@@ -3,10 +3,20 @@
  */
 package com.catalogue.moviecatalogueauthenticator.services;
 
+import com.catalogue.moviecatalogueauthenticator.dto.UserDTO;
+
+import java.util.List;
+
 /**
  * @author rohithvazhathody
  *
  */
 public interface UserService {
+    public List<UserDTO> getAllUsers();
 
+    public UserDTO getUserById(Long userId);
+
+    public UserDTO getUserByEmail(String userEmail);
+
+    public UserDTO findByUserName(String userName);
 }
---
+++ MovieCatalogueAuthenticator/src/main/java/com/catalogue/moviecatalogueauthenticator/services/UserServiceImpl.java
@@ -0,0 +1,46 @@
+package com.catalogue.moviecatalogueauthenticator.services;
+
+import com.catalogue.moviecatalogueauthenticator.dto.UserDTO;
+
+import java.util.List;
+
+/**
+ * @author priya prasad
+ */
+public class UserServiceImpl implements UserService {
+
+    /**
+     * @return
+     */
+    @Override
+    public List<UserDTO> getAllUsers() {
+        return null;
+    }
+
+    /**
+     * @param userId
+     * @return
+     */
+    @Override
+    public UserDTO getUserById(Long userId) {
+        return null;
+    }
+
+    /**
+     * @param userEmail
+     * @return
+     */
+    @Override
+    public UserDTO getUserByEmail(String userEmail) {
+        return null;
+    }
+
+    /**
+     * @param userName
+     * @return
+     */
+    @Override
+    public UserDTO findByUserName(String userName) {
+        return null;
+    }
+}
